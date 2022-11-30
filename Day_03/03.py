b1d7af2d4e8f2389ac1a107c2c8f771dd9367763
Kishan Kumar Rai <kishan.rai99693@gmail.com>
2022-11-30 23:13:29 +0530

    Create 03.py

---
+++ Day_03/03.py
@@ -0,0 +1,11 @@
+import pandas  # This is an example of external module
+import hashlib  # This is an example of built in module
+import webbrowser
+import pyautogui
+import tensorflow as tf
+
+print("Hi")
+
+# Dont worry about how to use these modules just yet!
+pandas.read_csv("one.csv")
+m = hashlib.sha256()
