bb51f187f018984f5efc8fad141cd4cf4a2432bc
Kishan Kumar Rai <kishan.rai99693@gmail.com>
2022-12-02 21:54:55 +0530

    Day 5

---
+++ Day_05/05.py
@@ -0,0 +1,28 @@
+#This is a 'Single-Line Comment'
+print("This is a print statement.")
+
+print("Hello World !!!") #Printing Hello World
+
+print("Python Program")
+#print("Python Program")
+
+#It will execute a block of code if a specified condition is true.
+#If the condition is false then it will execute another block of code.
+p = 7
+if (p > 5):
+    print("p is greater than 5.")
+else:
+    print("p is not greater than 5.")
+
+
+"""This is an if-else statement.
+It will execute a block of code if a specified condition is true.
+If the condition is false then it will execute another block of code."""
+p = 7
+if (p > 5):
+    print("p is greater than 5.")
+else:
+    print("p is not greater than 5.")
+
+# print("This doesnt "execute") this line throws an error
+print("This will \"execute")
