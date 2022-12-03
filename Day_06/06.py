6cde646e012aa7bec2a1fb625b57169f7fb758fb
Kishan Kumar Rai <kishan.rai99693@gmail.com>
2022-12-03 22:20:53 +0530

    Day 6

---
+++ Day_06/06.py
@@ -0,0 +1,32 @@
+a = complex(8, 2)
+b = True
+c = "Harry"
+d = None
+print("---------------------------------------")
+
+print(a)
+print(b)
+print(c)
+print(d)
+print("---------------------------------------")
+
+a1 = 9
+print(a + a1)
+print("---------------------------------------")
+
+print("The type of a is ", type(a))
+print("The type of b is ", type(b))
+print("The type of c is ", type(c))
+print("The type of c is ", type(d))
+print("---------------------------------------")
+
+list1 = [8, 2.3, [-4, 5], ["apple", "banana"]]
+print(list1)
+print("---------------------------------------")
+
+tuple1 = (("parrot", "sparrow"), ("Lion", "Tiger"))
+print(tuple1)
+print("---------------------------------------")
+
+dict1 = {"name":"Sakshi", "age":20, "canVote":True}
+print(dict1)
