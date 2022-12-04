79634d2e4e673fd4c34936f4e1f15020d369dfed
Kishan Kumar Rai <kishan.rai99693@gmail.com>
2022-12-04 21:49:58 +0530

    Day 7

---
+++ Day_07/07.py
@@ -0,0 +1,23 @@
+a = 6
+b = 5
+print("The value of a =",a)
+print("The value of b =", b)
+print("------------------------------------")
+
+print("The addition of", a, "&", b, "is", a+b)
+print("------------------------------------")
+
+print("The subtraction of", a, "&", b, "is", a-b)
+print("------------------------------------")
+
+print("The multiplication of", a, "&", b, "is", a*b)
+print("------------------------------------")
+
+print("The division of", a, "&", b, "is", a/b)
+print("------------------------------------")
+
+print("The floor division of", a, "&", b, "is", a//b)
+print("------------------------------------")
+
+print("The modulus of", a, "&", b, "is", a%b)
+print("------------------------------------")
