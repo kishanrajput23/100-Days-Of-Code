e66b822ddb2d920b82ee10e188ca06c0f73d5a9b
Kishan Kumar Rai <kishan.rai99693@gmail.com>
2022-12-08 22:19:47 +0530

    Day 11

---
+++ Day_11/11.py
@@ -0,0 +1,22 @@
+name = "Kishan"
+friend = "Rohit"
+anotherFriend = 'Manjeet'
+apple = '''He said, 
+Hi Harry
+hey I am good
+"I want to eat an apple'''
+ 
+print("Hello, " + name)
+
+# print(apple) 
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+
+# print(name[5]) # Throws an error
+
+print("Lets use a for loop\n")
+for character in apple:
+    print(character)
