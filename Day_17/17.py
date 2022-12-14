06d388607d564e2284ad303e74f7da56972eb682
Kishan Kumar Rai <kishan.rai99693@gmail.com>
2022-12-14 21:38:07 +0530

    Day 17

---
+++ Day_17/17.py
@@ -0,0 +1,19 @@
+name = 'Kishan'
+for i in name:
+  print(i)
+    
+colors = ["Red", "Green", "Blue", "Pink"]
+for color in colors:
+  print(color)
+  for i in color:
+    print(i)
+
+for k in range(5):
+  print(k + 1)
+  
+# for k in range(1, 20001):
+#   print(k)
+
+  
+for k in range(1, 12, 3):
+  print(k)
