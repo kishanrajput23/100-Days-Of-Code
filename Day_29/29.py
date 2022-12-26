806f3b46e17e27bdb29a91d6bad741244254cb07
Kishan Kumar Rai <kishan.rai99693@gmail.com>
2022-12-26 20:29:02 +0530

    Day 29

---
+++ Day_29/29.py
@@ -0,0 +1,38 @@
+def add(num1, num2):
+    """
+    Add up two integer numbers.
+
+    This function simply wraps the ``+`` operator, and does not
+    do anything interesting, except for illustrating what
+    the docstring of a very simple function looks like.
+
+    Parameters
+    ----------
+    num1 : int
+        First number to add.
+    num2 : int
+        Second number to add.
+
+    Returns
+    -------
+    int
+        The sum of ``num1`` and ``num2``.
+
+    See Also
+    --------
+    subtract : Subtract one integer from another.
+
+    Examples
+    --------
+    >>> add(2, 2)
+    4
+    >>> add(25, 0)
+    25
+    >>> add(10, -10)
+    0
+    """
+    return num1 + num2
+
+print("The sum of two numbers are", add(20, 10))
+print("The substraction of two numbers are", add(20, -10))
+print(add.__doc__)
