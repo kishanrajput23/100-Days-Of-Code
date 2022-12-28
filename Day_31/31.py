2cb5da00a1b99cf4a5ad2ce4401a3321a8f32a0e
Kishan Kumar Rai <kishan.rai99693@gmail.com>
2022-12-28 23:00:52 +0530

    Day 31

---
+++ Day_31/31.py
@@ -0,0 +1,23 @@
+s = {2, 4, 2, 6}
+print(s)
+
+info = {"Carla", 19, False, 5.9, 19}
+print(info)
+
+harry = set()
+print(type(harry))
+
+for value in info:
+  print(value)
+
+# create a set of integer type
+student_id = {112, 114, 116, 118, 115}
+print('Student ID:', student_id)
+
+# create a set of string type
+vowel_letters = {'a', 'e', 'i', 'o', 'u'}
+print('Vowel Letters:', vowel_letters)
+
+# create a set of mixed data types
+mixed_set = {'Hello', 101, -2, 'Bye'}
+print('Set of mixed data types:', mixed_set)
