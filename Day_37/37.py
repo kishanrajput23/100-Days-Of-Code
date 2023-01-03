9b7502509e2414d4ac9e049c484f15a9ab521c37
Kishan Kumar Rai <kishan.rai99693@gmail.com>
2023-01-03 23:37:33 +0530

    Day 37

---
+++ Day_37/37.py
@@ -0,0 +1,26 @@
+def func1():
+  try:
+    l = [1, 5, 6, 7]
+    i = int(input("Enter the index: "))
+    print(l[i])
+    return 1
+  except:
+    print("Some error occurred")
+    return 0
+
+  finally:
+    print("I am always executed")
+
+x = func1()
+print(x)
+
+print("---------------------------------------")
+
+try:
+    num = int(input("Enter an integer: "))
+except ValueError:
+    print("Number entered is not an integer.")
+else:
+    print("Integer Accepted.")
+finally:
+    print("This block is always executed.")
